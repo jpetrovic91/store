e568f71ae61d86a30f1ca8c78c211b0d143c2137
Josip Petrovic <josip.petrovic1991.jp@gmail.com>
2022-04-30 22:31:41 +0200

    liquibase added

--- src/main/java/com/josip/store/entities/StoreItem.java
+++ src/main/java/com/josip/store/entities/StoreItem.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -19,6 +16,7 @@ import javax.persistence.Table;
 public class StoreItem {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="item_id")
     private int itemId;
     @Column(name="item_name")
--- src/main/java/com/josip/store/repositories/StoreItemRepository.java
+++ src/main/java/com/josip/store/repositories/StoreItemRepository.java
@@ -4,8 +4,6 @@ import com.josip.store.entities.StoreItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface StoreItemRepository extends JpaRepository<StoreItem, Integer> {
 
